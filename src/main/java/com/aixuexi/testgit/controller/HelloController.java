9eb35113f61e5f856a024505ae0c4e6a36410234
薛硕 <xueshuo@aixuexi.com>
2020-07-19 20:58:54 +0800

    some code

---
+++ src/main/java/com/aixuexi/testgit/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.aixuexi.testgit.controller;
+
+/**
+ * @Auther:xue
+ * @Date:2020/7/19 20:57
+ */
+public class HelloController {
+
+    public String sayHello(){
+        System.out.println("hello");
+        return "success";
+    }
+}
